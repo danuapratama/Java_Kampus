af3a78b4d497325042051abfefd9a3c66c7c6bb7
danuapratama <danuaguspratama98@gmail.com>
2022-06-10 19:17:50 +0700

    first commit

---
+++ Payable_Interface/Payable.java
@@ -0,0 +1,9 @@
+// Fig. 10.11: Payable.java
+// Payable interface declaration.
+
+package latihanPBO2;
+
+public interface Payable {
+    double getPaymentAmount(); // calculate payment; no implementation
+} // end interface Payable                                           
+
